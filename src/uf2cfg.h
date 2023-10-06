c577c556fb3eee26e26959a803d07e521639a79f
文青·白雨木 <99241417+Byvm815@users.noreply.github.com>
2023-10-06 12:52:35 +0800

    更改链接

--- src/uf2cfg.h
+++ src/uf2cfg.h
@@ -1,6 +1,6 @@
 #define PRODUCT_NAME "STM32 Board"
 #define BOARD_ID "STM32F103-generic-v0"
-#define INDEX_URL "https://maker.makecode.com"
+#define INDEX_URL "https://github.com/Byvm815/uf2-stm32f103"
 #define UF2_NUM_BLOCKS 8000
 #define VOLUME_LABEL "USB Flash"
 // where the UF2 files are allowed to write data - we allow MBR, since it seems part of the softdevice .hex file
